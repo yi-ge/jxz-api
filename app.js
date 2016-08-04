8956d945f844489f37022ec75f09a9c1b0b70f7e
nixiang <237617561@qq.com>
2016-08-04 15:44:42 +0800

    init

---
+++ app.js
@@ -0,0 +1,50 @@
+import express from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
+
+import test from "./routes/test";
+
+const app = express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', test);
+
+// catch 404 and forward to error handler
+app.use((req, res, next)=> {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use((err, req, res, next)=> {
+        res.status(err.status || 500);
+        res.json({
+            code: err.status || 500,
+            msg: err.message
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use((err, req, res, next)=> {
+    res.status(err.status || 500);
+    res.json({
+        code: err.status || 500,
+        msg: err.message
+    });
+});
+
+module.exports = app;
