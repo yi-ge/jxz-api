73e812122eb02494366d7e6bd0e5b29668338056
nixiang <237617561@qq.com>
2016-08-08 11:52:21 +0800

    init

--- core/user/user.js
+++ core/user/user.js
@@ -6,8 +6,4 @@ import {User} from './../sequelize.config.js';
 
 let user = sequelize.define("user",User);
 
-user.sync({force: false}).then(function () {
-    // Table created
-    return user.create({});
-});
 export default user;
