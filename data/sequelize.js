2f254222bc60d65e575ced1aa0cc0cd078311adc
Hao <haovei@qq.com>
2016-08-31 13:50:58 +0800

    .

--- data/sequelize.js
+++ data/sequelize.js
@@ -5,17 +5,17 @@ import Sequelize from 'sequelize';
 import {dev239,localhost} from './database.config.json';
 
 const config = process.env.NODE_ENV ? {
-        host:process.env.dbHost,
-        port:process.env.dbPort,
-        dialect:process.env.dbDialect,
+    host: process.env.dbHost,
+    port: process.env.dbPort,
+    dialect: process.env.dbDialect,
 
-        database:process.env.dbName,
-        username:process.env.dbUsername,
-        password:process.env.dbPassword,
-    }:localhost;
+    database: process.env.dbName,
+    username: process.env.dbUsername,
+    password: process.env.dbPassword,
+} : localhost;
 const sequelize = new Sequelize(config.database, config.username, config.password, {
     host: config.host,
-    port:config.port || 3306,
+    port: config.port || 3306,
     dialect: config.dialect || "mysql",
     pool: {
         max: 5,
