b8bf2c1e9b75c2b0f7c6a17fb619f2d7a1a15570
nixiang <237617561@qq.com>
2016-09-06 14:53:49 +0800

    init

--- core/sysparameter/associated.config.js
+++ core/sysparameter/associated.config.js
@@ -2,5 +2,17 @@
  * Created by NX on 2016/8/18.
  */
 import SysParameter from './sysparameter';
-
+import SysUsers from './../sysusers';
+//字典关联一个创建人
+SysParameter.sequlize.belongsTo(SysUsers.sequlize, {
+    foreignKey: 'creater',
+    targetKey: 'id',
+    as: 'creater_user'
+});
+//字典关联一个修改人
+SysParameter.sequlize.belongsTo(SysUsers.sequlize, {
+    foreignKey: 'modifier',
+    targetKey: 'id',
+    as: 'modifier_user'
+});
 export default SysParameter;
