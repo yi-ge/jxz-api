a26d8baf8a28ed13e4313de68263b9451892dce7
nixiang <237617561@qq.com>
2016-09-19 17:02:32 +0800

    order

--- service/public/hotelrelevant/housesservice/housesservice.js
+++ service/public/hotelrelevant/housesservice/housesservice.js
@@ -38,6 +38,11 @@ class HousesService {
      */
     findHousesList(page, startDate, endDate, is_putaway, name, sys_id, pagesize) {
         let where = {};
+        if (endDate != void(0)) {
+            endDate = new Date(endDate);
+            endDate = endDate.setDate(endDate.getDate() + 1);
+            endDate = Houses.formatDate(endDate, 'yyyy-MM-dd');
+        }
         if (!!startDate && !!endDate) where['created_at'] = {$between: [startDate, endDate]};
         else if (!!startDate) where['created_at'] = {$gte: startDate};
         else if (!!endDate) where['created_at'] = {$lte: endDate};
